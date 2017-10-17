de40a95fa683008497102addd7658423d1d69e03
h-haraguchi <account.cgtdnpck@me.com>
2017-10-18 00:47:21 +0900

    コンソールにurlを出力するように変更。

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express'),
     http = require('http'),
     path = require('path'),
+    ip = require('ip'),
     app = express();
 app.configure(function () {
     app.set('port', process.env.PORT || 3000);
@@ -12,5 +13,6 @@ app.configure('development', function () {
 });
 
 http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server has started up.");
+    console.log("URL: http://" + ip.address() + ":" + app.get('port'));
 });
