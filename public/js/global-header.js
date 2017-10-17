fe37979fb68a68e07ab9e1959427964d3da393c8
h-haraguchi <account.cgtdnpck@me.com>
2017-10-17 23:47:50 +0900

    vueでなかった時代のファイル群を削除。

--- public/js/global-header.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var globalHeader = $(".global-header"),
-        menuTrigger = globalHeader.find(".menu-trigger"),
-        menuDetail = globalHeader.find('.menu-detail');
-    menuTrigger.on("click", function () {
-        menuTrigger.toggleClass('active');
-        menuDetail.toggle();
-    });
-}());
