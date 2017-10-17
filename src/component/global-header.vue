a5054ed4d25408e405ae4baf408cfd61de25dcf5
h-haraguchi <account.cgtdnpck@me.com>
2017-10-17 21:12:10 +0900

    グローバルヘッダーをvue.jsで表示できるようにする。
    vueifyでes6の構文つかうのにbabel-plugin-transform-runtimeが必要だったため追加。
    distに共用のjs/cssを追加。(要相談)

---
+++ src/component/global-header.vue
@@ -0,0 +1,139 @@
+<template>
+    <div class="global-header">
+        <div class="initial-display">
+            <div class="logo-area">
+                <a href="/" class="logo-link">MNR</a>
+            </div>
+            <div class="menu-area">
+                <div class="menu-trigger" v-bind:class="{active: isActive}" v-on:click="toggleMenu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+        <div class="menu-detail" v-if="isActive">
+            <ul class="global-header-links">
+                <li class="link-item">
+                    <a href="./undefined.html" class="menu-link">会社情報</a>
+                </li>
+                <li class="link-item">
+                    <a href="./undefined.html" class="menu-link">主な事業</a>
+                </li>
+                <li class="link-item">
+                    <a href="./undefined.html" class="menu-link">プレスリリース</a>
+                </li>
+                <li class="link-item">
+                    <a href="./undefined.html" class="menu-link">お問い合わせ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isActive: false
+            }
+        },
+        methods: {
+            toggleMenu() {
+                this.isActive = !this.isActive;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .global-header {
+        background-color: darkcyan;
+        color: white;
+    }
+
+    .initial-display {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .logo-area {
+        font-size: 30px;
+        font-family: Times, serif;
+        padding: 6px 12px;
+    }
+
+    .logo-link,
+    .logo-link:visited {
+        text-decoration: none;
+        color: white;
+    }
+
+    .menu-area {
+        padding: 12px;
+        height: 32px;
+    }
+
+    .menu-trigger,
+    .menu-trigger > span {
+        display: inline-block;
+        transition: all .4s;
+        box-sizing: border-box;
+    }
+
+    .menu-trigger {
+        position: relative;
+        width: 32px;
+        height: 32px;
+    }
+
+    .menu-trigger > span {
+        position: absolute;
+        width: 100%;
+        height: 4px;
+        border-radius: 4px;
+        background-color: white;
+    }
+
+    .menu-trigger > span:nth-of-type(1) {
+        top: 0;
+    }
+
+    .menu-trigger > span:nth-of-type(2) {
+        top: 14px;
+    }
+
+    .menu-trigger > span:nth-of-type(3) {
+        top: 28px;
+    }
+
+    .menu-trigger.active > span:nth-of-type(1) {
+        -webkit-transform: translateY(14px) rotate(-45deg);
+        transform: translateY(14px) rotate(-45deg);
+    }
+
+    .menu-trigger.active > span:nth-of-type(2) {
+        opacity: 0;
+    }
+
+    .menu-trigger.active > span:nth-of-type(3) {
+        -webkit-transform: translateY(-14px) rotate(45deg);
+        transform: translateY(-14px) rotate(45deg);
+    }
+
+    .global-header-links {
+        margin: 0;
+        list-style: none;
+        padding: 12px;
+    }
+
+    .menu-link {
+        color: white;
+        text-decoration: none;
+    }
+
+    .menu-link:visited {
+        color: white;
+    }
+
+</style>
--- src/component/index.vue
+++ src/component/index.vue
@@ -1,3 +1,15 @@
 <template>
-    <div>test</div>
+    <div id="index">
+        <global-header></global-header>
+    </div>
 </template>
+
+<script>
+    import GlobalHeader from './global-header.vue'
+
+    export default {
+        components: {
+            GlobalHeader
+        }
+    }
+</script>
