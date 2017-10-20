4bd18d1d97d5971a3d23aa79b289f3a241d3aea7
tudarom <tudarom@gmail.com>
2017-10-20 14:43:09 +0900

    indexページにて、ヒーローヘッダーを読み込ませてみた。

--- src/component/index.vue
+++ src/component/index.vue
@@ -1,15 +1,22 @@
 <template>
     <div id="index">
         <global-header></global-header>
+        <hero-header
+            img-source="https://www.pakutaso.com/shared/img/thumb/ransomwareIMGL1908_TP_V.jpg"
+            title="Index"
+            description="最高の価値をあなたに">
+        </hero-header>
     </div>
 </template>
 
 <script>
     import GlobalHeader from './global-header.vue'
+    import HeroHeader from './hero-header.vue'
 
     export default {
         components: {
-            GlobalHeader
+            GlobalHeader,
+            HeroHeader
         }
     }
 </script>
