cca9c6abac94c51e00b784220138337d4934f6f6
tudarom <tudarom@gmail.com>
2017-10-20 14:42:44 +0900

    WIP ヒーローヘッダの作成
    現在の課題として、descriptionの長さが50%になっている。
    個人的にmin-width: 文字サイズ、max-width:xx%みたいにしたい。

---
+++ src/component/hero-header.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="hero-header">
+        <div id="hero-img" v-bind:style="backGroundImgStyle()">
+            <div class="hero-description">{{ description }}</div>
+        </div>
+        <div id="hero-title">
+            <h1>{{ title }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        // backgroundのimageUrl
+        'imgSource': String,
+        // ページタイトル
+        'title': String,
+        // 画像内に収まる説明文. HACK:任意へ
+        'description': String,
+    },
+    methods: {
+        backGroundImgStyle() {
+            // HACK:imgSourceは必須にするかどうしよう
+            const imgUrl = (this.imgSource) ? this.imgSource :
+                 "https://www.pakutaso.com/shared/img/thumb/ransomwareIMGL1908_TP_V.jpg";
+            // url関数の結果を入れようとされてしまうため
+            const image = "url(" + imgUrl + ")";
+            return {
+                'background-image': image
+            };
+        }
+    }
+}
+</script>
+
+<style>
+    #hero-img {
+        background-position: 50% 50%;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 350px;
+    }
+
+    #hero-title {
+        text-align: center;
+    }
+
+    .hero-description {
+        background: rgba(255,255,255,0.5);
+        position: relative;
+        top: 70%;
+        margin-left: auto;
+        text-align: right;
+        padding: 5px;
+        width: 50%;
+    }
+</style>
