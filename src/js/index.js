f19b02a53c4e186e69377bf88978550fcd1f31ac
tronperidot <tronperidot@gmail.com>
2017-12-29 21:32:20 +0900

    ページ遷移時に、ページ変更前のスクロール位置を保持していた状態なので、トップに戻るように変更した。 戻る進むボタン押下時にはその位置にしている。

--- src/js/index.js
+++ src/js/index.js
@@ -23,7 +23,14 @@ new Vue({
                     { path: "product/vrc", component: Vrc },
                 ]
             },
-        ]
+        ],
+        scrollBehavior (_to, _from, savedPosition) {
+            if (savedPosition) {
+              return savedPosition
+            } else {
+              return { x: 0, y: 0 }
+            }
+          }
     }),
     render: h => h(App)
 });
