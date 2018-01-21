3020f6a43d69719c394142aaf9a598a06d969fe0
coq <maedaw1nb@gmail.com>
2018-01-21 16:25:59 +0900

    レビュー指摘の反映

--- src/component/contents/TheGlobalFooter.vue
+++ src/component/contents/TheGlobalFooter.vue
@@ -1,20 +1,12 @@
 <template>
     <div id="the-global-footer">
-        <div id="block">
-            <h3>東京本店</h3>
-            <p>{{addrTokyo}}</p>
-            <a id="button-google-map" href="https://goo.gl/maps/6zX1Z1SQd9m">Google Map</a>
-        </div>
-        <div id="block">
-            <h3>大阪支店</h3>
-            <p>{{addrOsaka}}</p>
-            <a id="button-google-map" href="https://goo.gl/maps/8uJoUZgodmw">Google Map</a>
-        </div>
-        <div id="block">
-            <h3>シンガポール支店</h3>
-            <p>{{addrSingapore}}</p>
-            <a id="button-google-map" href="https://goo.gl/maps/7WpzZVpXToq">Google Map</a>
-        </div>
+        <li v-for="item in shops" :key="item.name">
+            <div class="shop-block">
+                <h3>{{ item.name }}</h3>
+                <p>{{ item.addr }}</p>
+                <a id="button-google-map" v-bind:href="item.href">Google Map</a>
+            </div>
+        </li>
         <div id="copyright">{{copyright}}</div>
     </div>
 </template>
@@ -22,10 +14,25 @@
 <script>
 export default {
     data(){
+        const shops = [
+            {
+                name: "東京本店",
+                addr: "〒156-0052 東京都世田谷区経堂１丁目１２−６ 城南信用金庫隣り",
+                href: "https://goo.gl/maps/6zX1Z1SQd9m"
+            },
+            {
+                name: "大阪支店",
+                addr: "〒542-0085 大阪府大阪市中央区心斎橋筋２丁目１−３０ プラディオ心斎橋 2F",
+                href: "https://goo.gl/maps/8uJoUZgodmw"
+            },
+            {
+                name: "シンガポール支店",
+                addr: "71, Jalan Austin Heights 8/1, Taman Mount Austin, 81100 Johor Bahru, Johor, マレーシア",
+                href: "https://goo.gl/maps/7WpzZVpXToq"
+            }
+        ]
         return{
-            addrTokyo: "〒156-0052 東京都世田谷区経堂１丁目１２−６ 城南信用金庫隣り",
-            addrOsaka: "〒542-0085 大阪府大阪市中央区心斎橋筋２丁目１−３０ プラディオ心斎橋 2F",
-            addrSingapore: "71, Jalan Austin Heights 8/1, Taman Mount Austin, 81100 Johor Bahru, Johor, マレーシア",
+            shops: shops,
             copyright: "Copyright© 2018 MNR Imp. All rights reserved to the devil."
         }
     }
@@ -37,18 +44,16 @@ export default {
         background-color: #a1a1a1;
         text-align: center;
         padding: 1em;
+        list-style:none;
     }
     #button-google-map {
         padding:10px 30px 10px 40px !important;
         background: #0098d9 url(./img/map.png) no-repeat 9% center/18px;
     }
-    #block {
+    .shop-block {
         margin: 0px 0px 1em 0px;
         padding: 1em;
         background-color: darkcyan;
         border-radius: 1em;
-    }
-    #copyright {
-
     }
 </style>
