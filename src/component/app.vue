c8d5cffec8db29afaa25d44cff2f162c8c12e078
tronperidot <tronperidot@gmail.com>
2018-09-15 16:14:57 +0900

    add indicator plugin.

--- src/component/app.vue
+++ src/component/app.vue
@@ -5,3 +5,4 @@
 <style src="../css/normalize.css"></style>
 <style src="../css/main.css"></style>
 <style src="../css/theme.css"></style>
+<style src="../../node_modules/vue-loading-overlay/dist/vue-loading.min.css"></style>
